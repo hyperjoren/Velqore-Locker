f95e29ca3fc6a16b090a8ce1d930ae664f746d5f
Hyper Joren <hyperjoren@gmail.com>
2026-05-18 18:24:27 +0530

    Update info.py
    Signed-off-by: Hyper Joren <hyperjoren@gmail.com>

--- info.py
+++ info.py
@@ -89,10 +89,10 @@ BATCH_LIMIT = int(environ.get('BATCH_LIMIT', 60))
 # =========================================================
 # 🖼️ MEDIA & CAPTIONS
 # =========================================================
-QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/6afb4093d5ec5c4176979.jpg')
+QR_CODE = environ.get('QR_CODE', 'https://ibb.co/p6qVjG8j')
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
-AUTH_PICS = environ.get('AUTH_PICS', 'https://envs.sh/AwV.jpg')
-PICS = environ.get('PICS', 'https://ibb.co/VpTJNNCN')
+AUTH_PICS = environ.get('AUTH_PICS', 'https://ibb.co/spnkR7Pj')
+PICS = environ.get('PICS', 'https://ibb.co/spnkR7Pj')
 FILE_PIC = environ.get('FILE_PIC', 'https://i.ibb.co/bj4My0bW/photo-2025-07-21-02-15-21-7529360175656861700.jpg')
 
 FILE_CAPTION = environ.get('FILE_CAPTION', script.CAPTION)
